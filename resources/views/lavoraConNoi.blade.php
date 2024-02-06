4c67c0463cd9ae7b29991c826daefbc1c5b0999a
Anthony.Elia <anthony.elia@libero.it>
2024-02-06 22:14:51 +0100

    first commit

---
+++ resources/views/lavoraConNoi.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lavora con noi</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light  p-3">
+        <a class="navbar-brand" href="#">Laravel</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+    <a class="nav-item nav-link active" href="/homepage">Home<span class="sr-only"></span></a>
+        <a class="nav-item nav-link" href="/lavoraConNoi">Lavora con noi</a>
+        <a class="nav-item nav-link" href="/chiSiamo">Chi siamo</a>
+        <a class="nav-item nav-link" href="/Contatti">Contatti</a>
+    </div>
+</div>
+</nav>
+<h1 class="text-center text-danger">Lavora con noi</h1>
+<div class="text-center">
+    <a href="/chiSiamo" class="text-danger">Chi siamo</a>
+    <a href="/homepage" class="text-danger">Homepage</a>
+</div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</html>
